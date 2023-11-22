feec0afa5a43c6cd7dda331ca401daddf875576e
email <szbfrnc7@gmail.com>
2023-11-22 20:04:59 +0100

    index_modification

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
 //alkalmaz�s gy�k�r k�nyvt�ra a szerveren
-define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT'].'/FEHGUP_web/');
+define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT'].'/FEHGUP_web2-1/');
 
 //URL c�m az alkalmaz�s gy�ker�hez
-define('SITE_ROOT', 'http://localhost/FEHGUP_web/');
+define('SITE_ROOT', 'http://localhost/FEHGUP_web2-1/');
 
 // a router.php vez�rl� bet�lt�se
 require_once(SERVER_ROOT . 'controllers/' . 'router.php');
